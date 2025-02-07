54d5772da315b1f0c1f1cbb46eb1d23cd50a608b
冰糖xh <nine826@outlook.com>
2025-02-07 10:01:32 +0800

    完善了 DPIDDC ，现在可以双击运行了

--- DPIDDC.c
+++ DPIDDC.c
@@ -1,95 +1,132 @@
 #include <stdio.h>
 #include <string.h>
 #include <windows.h>
+#include <winreg.h>
 #pragma comment(lib,"User32.lib")
+#pragma comment(lib,"Advapi32.lib")
 
 const char KEY_CHARSET[] = "BCDFGHJKMPQRTVWXY2346789";
 
-int GetHexVal(const char hex)
+int getHexVal(const char hex)
 {
-	return hex - ((hex < ':') ? 48 : ((hex < 'a') ? 55 : 87));
+	return hex-((hex>'9')?((hex<'a')?55:87):48);
 }
 
-int StringToByteArrayFastest(const char * hex,char * result)
+int string2ByteArrayFastest(const char * hex,char * result)
 {
-	if (strlen(hex) % 2 == 1)
+	if (strlen(hex)%2==0)
 	{
-		puts("The binary key cannot have an odd number of digits");
+        for (int i=0;i<(int)strlen(hex)/2;i++)
+        {
+            result[i]=(char) ( (getHexVal(hex[i*2])*16) + getHexVal(hex[(i*2)+1]) );
+        }
+        return 0;
+    }
+    else
+    {
+        puts(hex);
+		puts("十六进制的原字符串的长度不能是奇数");
         return -1;
-	}
-	// byte[] array = new byte[hex.Length >> 1];
-	for (int i = 0; i < (int) strlen(hex) >> 1; i++)
-	{
-		result[i] = (char)((GetHexVal(hex[i << 1]) << 4) + GetHexVal(hex[(i << 1) + 1]));
-	}
-	return 0;
+    }
 }
 
 void decodePID(const unsigned char *digitalProductId, char *productKey) {
-    int keyStart = 52;
-    int keyEnd = keyStart + 15;
     int num, temp;
     unsigned char pidBlock[15];
     char decodedKey[29];
+    int isNKey;
 
     // 复制 PID 相关的 15 个字节
-    for (int i = keyStart; i <= keyEnd; i++) {
-        pidBlock[i - keyStart] = digitalProductId[i];
+    for (int i=52;i<=67;i++) {
+        pidBlock[i-52]=digitalProductId[i];
     }
 
     // 特殊位操作
-    int isNKey = (pidBlock[14] >> 3) & 1;
-    pidBlock[14] = (pidBlock[14] & 0xF7) | ((isNKey & 2) << 2);
+    isNKey=(pidBlock[14]/8)%2;
+    pidBlock[14]=(pidBlock[14]&247)|((isNKey%4>=2?2:0)*4);
 
     // 初始化 Key 结果数组
-    memset(decodedKey, 0, sizeof(decodedKey));
+    memset(decodedKey,0,sizeof(decodedKey));
 
     // Base24 解码
-    for (int i = 28; i >= 0; i--) {
-        if ( ( (i + 1) % 6 == 0 ) && 1 ) {
-            decodedKey[i] = '-';
+    for (int i=28;i>=0;i--) {
+        if (((i+1)%6==0)&&1) {
+            decodedKey[i]='-';
         } else {
-            num = 0;
-            for (int j = 14; j >= 0; j--) {
-                temp = (num << 8) | pidBlock[j];
-                pidBlock[j] = temp / 24;
-                num = temp % 24;
-                decodedKey[i] = KEY_CHARSET[num];
+            num=0;
+            for (int j=14;j>=0;j--) {
+                temp=(num*256)|pidBlock[j];
+                pidBlock[j]=temp/24;
+                num=temp%24;
+                decodedKey[i]=KEY_CHARSET[num];
             }
-            
         }
     }
     // puts(decodedKey);
     // 处理 N 版本密钥
-    if (isNKey) {
-        int nPos = 0;
-        for (int i = 0; i < 24; i++) {
-            if (decodedKey[0] == KEY_CHARSET[i]) {
-                nPos = i;
+    if (isNKey!=0)
+    {
+        int nPos=0;
+        for (int i=0;i<24;i++) {
+            if (decodedKey[0]==KEY_CHARSET[i]) {
+                nPos=i;
                 break;
             }
         }
         //memmove(decodedKey, decodedKey + 1, 28);
         // puts(decodedKey);
-        decodedKey[29] = '\0';
-        decodedKey[nPos] = 'N';
+        decodedKey[29]='\0';
+        decodedKey[nPos]='N';
     }
     // puts(decodedKey);
     // 格式化输出
-    snprintf(productKey, 30, "%5.5s-%5.5s-%5.5s-%5.5s-%5.5s",
-             decodedKey, decodedKey + 6, decodedKey + 12, decodedKey + 18, decodedKey + 24 );
+    snprintf(productKey,30,"%5.5s-%5.5s-%5.5s-%5.5s-%5.5s",
+             decodedKey,decodedKey+6,decodedKey+12,decodedKey+18,decodedKey+24);
 }
 
 int main(int argc,char **argv)
 {
-    char rawInput[329];
-    unsigned char digitalProductId[164];
-    char productKey[30];
-    if(argc==1) {
-        MessageBox(NULL,"本程序只能从命令行运行，使用 /? 查看用法。","DPIDDC",MB_ICONINFORMATION + MB_OK);
-        return 1;
+    for(int i=0;i<256;i++)
+    {
+        printf("%d\n",i&247);
     }
-    if(argc==3 || argc==2) {
+    HKEY hKey;
+    DWORD size=ULONG_MAX;
+    char buf[UCHAR_MAX];
+    char rawInput[329];
+    char productKey[30];
+    char result[154]="本程序也可以从命令行运行，使用 /? 查看用法。\n你的产品密钥为： ";
+    char command[41];
+    unsigned char digitalProductId[164];
+    long unsigned int lpType=REG_BINARY;
+    if(argc==1) {
+        puts("Digital Product Id Decoder Console Edition");
+        puts("");
+        puts("Copyright (C) bingtangxh.");
+        puts("");
+        long ORet=RegOpenKeyEx(HKEY_LOCAL_MACHINE,"SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion",0,KEY_READ,&hKey);
+        if (ORet!=ERROR_SUCCESS)
+        {
+            printf("打开注册表失败：\n%s\n%s\n","SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion","DigitalProductId");
+            return 3;
+        }
+        else
+        {
+            long QRet=RegQueryValueEx(hKey,"DigitalProductId",0,&lpType,(BYTE*)buf, &size);
+            decodePID(buf, productKey);
+            strncat_s(result,154,productKey,29);
+            strncat_s(result,154,"\n要将它复制到剪贴板吗？",35);
+            RegCloseKey(hKey);
+        }
+
+        if(MessageBox(NULL,result,"DPIDDC",MB_ICONINFORMATION + MB_YESNO)==IDYES)
+        {
+            snprintf(command,41,"echo %s| clip",productKey);
+            system(command);
+        }
+        return 0;
+    }
+    if(argc==3||argc==2) {
         if(!_stricmp(argv[1],"/venti"))
         {
             if(strlen(argv[2])==328)
@@ -110,14 +147,16 @@ int main(int argc,char **argv)
         {
             puts("将 DigitalProductId 注册表 DWORD 数值解码得到产品密钥。");
             puts("");
+            puts("Copyright (C) bingtangxh.");
+            puts("");
             puts("DPIDDC [ /VENTI digitalproductid | /? ]");
             puts("");
-            puts("/VENTI             表示进行解码。");
+            puts("/VENTI             表示进行解码用户给定的 DigitalProductId。");
             puts("digitalproductid   注册表中长度为 328 的 DWORD 数值。");
             puts("");
             puts("/?                 显示此帮助信息。");
             puts("");
-            puts("无法直接双击（不带任何参数）运行本程序。");
+            puts("也可以直接双击（不带任何参数）运行本程序，只能查询当前系统的产品密钥。");
             puts("");
             puts("如果想要从一个批处理脚本调用本程序，你可以这样使用：");
             puts("");
@@ -130,9 +169,8 @@ int main(int argc,char **argv)
         puts("参数错误，使用 /? 查看用法。");
         return 1;
     }
-    StringToByteArrayFastest(rawInput,digitalProductId);
+    string2ByteArrayFastest(rawInput,digitalProductId);
     decodePID(digitalProductId, productKey);
-    //printf("Decoded Product Key: %s\n", productKey);
     puts(productKey);
     return 0;
 }
